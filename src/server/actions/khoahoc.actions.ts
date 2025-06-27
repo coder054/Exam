b8db38c8219033a2c37d669178d3bd7292d53246
anhdt <tuananh92.ftu@gmail.com>
2025-06-27 16:50:48 +0700

    debug

--- src/server/actions/khoahoc.actions.ts
+++ src/server/actions/khoahoc.actions.ts
@@ -37,37 +37,43 @@ interface GetKetQuaThiFormState {
 
 export async function getKetQuaThi(
   _formState: GetKetQuaThiFormState,
-  formData: FormData,
+  _formData: FormData,
 ): Promise<GetKetQuaThiFormState> {
-  const kyThiId = formData.get("kyThiId") as string;
-  const sbd = formData.get("sbd") as string;
-  // await new Promise((res) => setTimeout(res, 2000));
-  const thiSinh = await db.thiSinh.findFirst({
-    where: {
-      soBaoDanh: String(sbd),
-    },
-  });
-  if (!thiSinh) {
-    return {
-      errors: { message: "khong tim thay thi sinh" },
-      data: [],
-    };
-  }
-
-  const app = await db.application.findMany({
-    where: {
-      thiSinhId: thiSinh.id,
-      kyThiId: parseInt(String(kyThiId)),
-    },
-
-    include: {
-      candidate: true, // ThiSinh
-      kyThi: true, // KyThi
-    },
-  });
+  await new Promise((res) => setTimeout(res, 2000));
 
   return {
-    errors: {},
-    data: app || [],
+    errors: { message: "aaa test message" },
+    data: [],
   };
+
+  // const kyThiId = _formData.get("kyThiId") as string;
+  // const sbd = _formData.get("sbd") as string;
+  // const thiSinh = await db.thiSinh.findFirst({
+  //   where: {
+  //     soBaoDanh: String(sbd),
+  //   },
+  // });
+  // if (!thiSinh) {
+  //   return {
+  //     errors: { message: "khong tim thay thi sinh" },
+  //     data: [],
+  //   };
+  // }
+
+  // const app = await db.application.findMany({
+  //   where: {
+  //     thiSinhId: thiSinh.id,
+  //     kyThiId: parseInt(String(kyThiId)),
+  //   },
+
+  //   include: {
+  //     candidate: true, // ThiSinh
+  //     kyThi: true, // KyThi
+  //   },
+  // });
+
+  // return {
+  //   errors: {},
+  //   data: app || [],
+  // };
 }
