fc1f5d58dd96a7607bba82ff84fcb58afe2736e0
anhdt <tuananh92.ftu@gmail.com>
2025-06-27 16:56:03 +0700

    debug 2

--- src/server/actions/khoahoc.actions.ts
+++ src/server/actions/khoahoc.actions.ts
@@ -1,6 +1,7 @@
 "use server";
 
 import { db } from "../db";
+import { formatError } from "~/lib/utils";
 
 export type TDanhSachDuThi = Awaited<
   ReturnType<typeof getListApplicationKhoaHocVBTuyenMoi>
@@ -39,41 +40,51 @@ export async function getKetQuaThi(
   _formState: GetKetQuaThiFormState,
   _formData: FormData,
 ): Promise<GetKetQuaThiFormState> {
-  await new Promise((res) => setTimeout(res, 2000));
-
-  return {
-    errors: { message: "aaa test message" },
-    data: [],
-  };
-
-  // const kyThiId = _formData.get("kyThiId") as string;
-  // const sbd = _formData.get("sbd") as string;
-  // const thiSinh = await db.thiSinh.findFirst({
-  //   where: {
-  //     soBaoDanh: String(sbd),
-  //   },
-  // });
-  // if (!thiSinh) {
-  //   return {
-  //     errors: { message: "khong tim thay thi sinh" },
-  //     data: [],
-  //   };
-  // }
-
-  // const app = await db.application.findMany({
-  //   where: {
-  //     thiSinhId: thiSinh.id,
-  //     kyThiId: parseInt(String(kyThiId)),
-  //   },
-
-  //   include: {
-  //     candidate: true, // ThiSinh
-  //     kyThi: true, // KyThi
-  //   },
-  // });
+  // await new Promise((res) => setTimeout(res, 2000));
 
   // return {
-  //   errors: {},
-  //   data: app || [],
+  //   errors: { message: "aaa test message" },
+  //   data: [],
   // };
+  try {
+    const kyThiId = _formData.get("kyThiId") as string;
+    const sbd = _formData.get("sbd") as string;
+    const thiSinh = await db.thiSinh.findFirst({
+      where: {
+        soBaoDanh: String(sbd),
+      },
+    });
+    if (!thiSinh) {
+      return {
+        errors: { message: "khong tim thay thi sinh" },
+        data: [],
+      };
+    }
+
+    const app = await db.application.findMany({
+      where: {
+        thiSinhId: thiSinh.id,
+        kyThiId: parseInt(String(kyThiId)),
+      },
+
+      include: {
+        candidate: true, // ThiSinh
+        kyThi: true, // KyThi
+      },
+    });
+
+    return {
+      errors: {
+        message: "",
+      },
+      data: app || [],
+    };
+  } catch (error) {
+    return {
+      errors: {
+        message: formatError(error),
+      },
+      data: [],
+    };
+  }
 }
