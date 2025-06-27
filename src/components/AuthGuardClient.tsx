fe2e56ce3adc7c681ebf19f4adf23d911ca8d9ba
anhdt <tuananh92.ftu@gmail.com>
2025-06-27 22:12:45 +0700

    check role admin

--- src/components/AuthGuardClient.tsx
+++ src/components/AuthGuardClient.tsx
@@ -5,12 +5,13 @@ import { useRouter } from "next/navigation";
 import { authClient } from "./../lib/auth-client";
 import type { ReactNode } from "react";
 import { ROUTES } from "~/constants";
-import LoadingPage from "~/app/loading";
 
 export default function AuthGuardClient({ children }: { children: ReactNode }) {
   const router = useRouter();
   const sessionResponse = authClient.useSession();
   const { data: session, isPending } = sessionResponse;
+  const role = (session?.user as any)?.role;
+  console.log("aaa roleeee", role);
 
   // Wait until we know the auth status before rendering
   const loading = isPending === true;
@@ -19,10 +20,13 @@ export default function AuthGuardClient({ children }: { children: ReactNode }) {
     if (!loading && !session?.user) {
       router.push(ROUTES.signIn);
     }
+    if (!loading && role !== "ADMIN") {
+      router.push(ROUTES.home);
+    }
   }, [loading, session, router]);
 
   if (loading) {
-    return <LoadingPage />;
+    return null;
   }
 
   // Don't show anything (not even 'unauthorized') while redirecting
