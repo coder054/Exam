344ebd8d27cc42769d49b2ddfb30dd807e61729c
anhdt <tuananh92.ftu@gmail.com>
2025-06-27 12:39:08 +0700

    update seed

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -27,7 +27,7 @@ async function main() {
   });
 
   // Tạo thí sinh
-  const thiSinhKyThiVB2_1 = await db.thiSinh.create({
+  const anhvb012 = await db.thiSinh.create({
     data: {
       hoTen: "Nguyễn Văn A",
       soBaoDanh: "ANHVB012",
@@ -35,14 +35,14 @@ async function main() {
     },
   });
 
-  const thiSinhKyThiVB2_2 = await db.thiSinh.create({
+  const anhvb037 = await db.thiSinh.create({
     data: {
       hoTen: "Nguyễn Văn B",
       soBaoDanh: "ANHVB037",
       CCCD: "212121212133",
     },
   });
-  const thiSinhKyThiVB2_3 = await db.thiSinh.create({
+  const anhvb050 = await db.thiSinh.create({
     data: {
       hoTen: "Nguyễn Văn C",
       soBaoDanh: "ANHVB050",
@@ -53,7 +53,7 @@ async function main() {
   await db.application.create({
     data: {
       soThuTu: 1,
-      thiSinhId: thiSinhKyThiVB2_1.id,
+      thiSinhId: anhvb012.id,
       kyThiId: kyThiVB2TuyenMoi.id,
       nganh: 7860100,
       vung: 1,
@@ -69,7 +69,7 @@ async function main() {
   await db.application.create({
     data: {
       soThuTu: 2,
-      thiSinhId: thiSinhKyThiVB2_1.id,
+      thiSinhId: anhvb012.id,
       kyThiId: kyThiVB2TuyenMoi.id,
       nganh: 7860100,
       vung: 1,
@@ -85,7 +85,7 @@ async function main() {
   await db.application.create({
     data: {
       soThuTu: 3,
-      thiSinhId: thiSinhKyThiVB2_2.id,
+      thiSinhId: anhvb037.id,
       kyThiId: kyThiVB2TuyenMoi.id,
       nganh: 7480202,
       vung: 9,
@@ -101,7 +101,7 @@ async function main() {
   await db.application.create({
     data: {
       soThuTu: 4,
-      thiSinhId: thiSinhKyThiVB2_3.id,
+      thiSinhId: anhvb050.id,
       kyThiId: kyThiVB2TuyenMoi.id,
       nganh: 7860100,
       vung: 1,
@@ -111,6 +111,92 @@ async function main() {
       diemBaiThiTuLuan2: null,
       maBaiThi: null,
       tongDiemXetTuyen: null,
+      ketQua: "PASSED",
+    },
+  });
+
+  await db.application.create({
+    data: {
+      soThuTu: 1,
+      thiSinhId: anhvb012.id,
+      kyThiId: kyThiD57TuyenHSTHPT.id,
+      nganh: 7860100,
+      vung: 1,
+      phuongThuc: "METHOD_1",
+      diemCong: null,
+      diemBaiThiTuLuan1: null,
+      diemBaiThiTuLuan2: null,
+      maToHop: "A00",
+      maBaiThi: null,
+      tongDiemXetTuyen: null,
+      ketQua: "NOT_PASSED",
+    },
+  });
+  await db.application.create({
+    data: {
+      soThuTu: 2,
+      thiSinhId: anhvb012.id,
+      kyThiId: kyThiD57TuyenHSTHPT.id,
+      nganh: 7860100,
+      vung: 1,
+      phuongThuc: "METHOD_2",
+      diemCong: 2,
+      diemBaiThiTuLuan1: 25,
+      diemBaiThiTuLuan2: 55,
+      maToHop: "A01",
+      maBaiThi: "CA1",
+      tongDiemXetTuyen: 26.0,
+      ketQua: "PASSED",
+    },
+  });
+  await db.application.create({
+    data: {
+      soThuTu: 3,
+      thiSinhId: anhvb037.id,
+      kyThiId: kyThiD57TuyenHSTHPT.id,
+      nganh: 7480202,
+      vung: 9,
+      phuongThuc: "METHOD_2",
+      diemCong: null,
+      diemBaiThiTuLuan1: 25,
+      diemBaiThiTuLuan2: 55,
+      maToHop: "D01",
+      maBaiThi: "CA1",
+      tongDiemXetTuyen: 25.0,
+      ketQua: "NOT_PASSED",
+    },
+  });
+  await db.application.create({
+    data: {
+      soThuTu: 4,
+      thiSinhId: anhvb050.id,
+      kyThiId: kyThiD57TuyenHSTHPT.id,
+      nganh: 7860100,
+      vung: 1,
+      phuongThuc: "METHOD_1",
+      diemCong: null,
+      diemBaiThiTuLuan1: null,
+      diemBaiThiTuLuan2: null,
+      maToHop: "C03",
+      maBaiThi: null,
+      tongDiemXetTuyen: null,
+      ketQua: "PASSED",
+    },
+  });
+  await db.application.create({
+    data: {
+      soThuTu: 5,
+      thiSinhId: anhvb050.id,
+      kyThiId: kyThiD57TuyenHSTHPT.id,
+      nganh: 7860100,
+      vung: 1,
+      phuongThuc: "METHOD_3",
+      diemCong: null,
+      diemBaiThiTuLuan1: 25,
+      diemBaiThiTuLuan2: 55,
+      maToHop: "C03",
+      maBaiThi: "CA4",
+      tongDiemXetTuyen: 27.0,
       ketQua: "PASSED",
     },
   });
