b899835596b421f4093796089196193e7609bdc9
anhdt <tuananh92.ftu@gmail.com>
2025-06-28 10:53:16 +0700

    add debug page

---
+++ src/app/test/page.tsx
@@ -0,0 +1,15 @@
+import { env } from "~/env";
+
+export default function PageTest() {
+  return (
+    <>
+      <div className=" ">
+        NEXT_PUBLIC_SERVER_URL: {env.NEXT_PUBLIC_SERVER_URL}
+      </div>
+      <div className=" ">NODE_ENV: {env.NODE_ENV}</div>
+      <div className=" ">BETTER_AUTH_SECRET: {env.BETTER_AUTH_SECRET}</div>
+      <div className=" ">BETTER_AUTH_URL: {env.BETTER_AUTH_URL}</div>
+      <div className=" ">DATABASE_URL: {env.DATABASE_URL}</div>
+    </>
+  );
+}
